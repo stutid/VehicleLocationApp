584af0b645d8859c2f9727a0e009f84747e404ed
Stuti <stuti.dobhal@algoworks.com>
2019-07-05 09:52:08 +0530

    Initial Commit

---
+++ VehicleLocationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VehicleLocationApp
+//
+//  Created by Stuti on 05/07/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
